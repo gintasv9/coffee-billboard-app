0e8667b330f3895d57f2b1fe72424c675c9e0602
Gintautas Vasauskas <gintasv9@gmail.com>
2019-03-13 23:47:09 +0200

    Updates

---
+++ precache-manifest.01148fac5ece08b52be24b11d2757037.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/coffee-billboard-app/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "319ae6de46fe8649917f",
+    "url": "/coffee-billboard-app/static/css/main.2c5b521c.chunk.css"
+  },
+  {
+    "revision": "d857d04b1e39ff7568dc",
+    "url": "/coffee-billboard-app/static/js/runtime~main.e94aeda1.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/coffee-billboard-app/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c80a2ee66e3b0f7a55a9",
+    "url": "/coffee-billboard-app/static/js/2.a9b783e4.chunk.js"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/coffee-billboard-app/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/coffee-billboard-app/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/coffee-billboard-app/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/coffee-billboard-app/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/coffee-billboard-app/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/coffee-billboard-app/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "319ae6de46fe8649917f",
+    "url": "/coffee-billboard-app/static/js/main.a442ab80.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/coffee-billboard-app/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/coffee-billboard-app/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/coffee-billboard-app/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/coffee-billboard-app/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/coffee-billboard-app/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/coffee-billboard-app/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/coffee-billboard-app/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/coffee-billboard-app/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "c80a2ee66e3b0f7a55a9",
+    "url": "/coffee-billboard-app/static/css/2.6d144516.chunk.css"
+  },
+  {
+    "revision": "e4050b0474529c091da071f594e8f85f",
+    "url": "/coffee-billboard-app/index.html"
+  }
+];
